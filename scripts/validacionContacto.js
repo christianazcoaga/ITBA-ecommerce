496b8b973d788f932f149d2b021cd6cf1db18537
matias-or <matiasbibilaquad@gmail.com>
2025-09-07 00:06:55 -0300

    validación del formulario de contacto

---
+++ scripts/validacionContacto.js
@@ -0,0 +1,41 @@
+// inputs en el formulario de contactos
+const nombreUsuario = document.querySelector('#nombreUsuario');
+const emailUsuario = document.querySelector('#emailUsuario');
+const mensajeUsuario = document.querySelector('#mensajeUsuario');
+
+// formulario y mensajes de error
+const formulario = document.querySelector('#formularioContacto');
+const msgErrorEmail = document.querySelector('#msgErrorEmail');
+const msgErrorMensaje = document.querySelector('#msgErrorMensaje');
+
+function validarEmail (email) {
+    const emailPatron = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+    return emailPatron.test(email);
+}
+
+formulario.addEventListener('submit', function(evento){
+    // prevenir que se envie el formulario
+    evento.preventDefault();
+
+    // valores ingresados por los usuarios
+    const email = emailUsuario.value;
+    const mensaje = mensajeUsuario.value;
+    let esValido = true;
+
+    if (!validarEmail(email)) {
+        msgErrorEmail.textContent = 'Por favor, ingrese un email válido.';
+        esValido = false;
+    }
+
+    if (mensaje.trim().length < 5) {
+        msgErrorMensaje.textContent = 'Por favor, ingrese al menos 5 carácteres.';
+        esValido = false;
+    }
+
+    if (esValido) {
+        formulario.submit();
+        alert('¡Formulario enviado con éxito!');
+    }
+
+
+})
